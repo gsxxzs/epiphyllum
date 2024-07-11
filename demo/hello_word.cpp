788208b495e6ccb130856c68ccbca1df2de7ea79
xxzs <1843556251@qq.com>
2024-07-11 02:41:49 -0700

    add cpm

--- demo/hello_word.cpp
+++ demo/hello_word.cpp
@@ -2,12 +2,21 @@
 #include <thread>
 #include <unistd.h>
 #include <util/log.h>
+// #include "yaml-cpp/yaml.h"
 
 int main(){
+  // std::string input = R"(  
+  //       a: 1  
+  //       b:  
+  //         c: 2  
+  //         d: [3, 4]  
+  //   )";  
+  
+  // auto doc = YAML::Load(input);
+
   epiphiyllum::Config config;
   log_init(config);
-  LOG_INFO << "hello world " << 1;
-  LOG_WARN << "hello world " << 2;
-  LOG_ERROR << "hello world " << 3;
+  LOG_WARN << "hello world";
+  // LOG_INFO << doc["a"].as<std::string>();
   return 0;
 }
---
+++ extern_dep.cmake
@@ -0,0 +1,9 @@
+include(CPM.cmake)
+
+# CPMAddPackage(
+#   NAME yaml-cpp
+#   GIT_TAG 0.8.0
+#   GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+# )
+# include_directories(${yaml-cpp_SOURCE_DIR}/include)
+# link_directories(${yaml-cpp__SOURCE_DIR}/lib)
